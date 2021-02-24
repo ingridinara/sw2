969472e224b169c741f3106e08bbef9f579ad267
ingridinara <ingridinara@me.com>
2021-02-24 16:04:53 +0100

    1. Applied styles to Character List

--- src/CharacterList.js
+++ src/CharacterList.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-
+import { PageStyled, PageTitle, CharactersNames } from './characterliststyled';
 export default class CharacterList extends Component {
   render() {
     const characters = this.props.characters;
     return (
-      <div>
+      <PageStyled>
+        <PageTitle>Characters List</PageTitle>
         {characters.map((c) => {
           console.log(c);
           return (
             <div key={c.url}>
-              <p>Characters List</p>
-              <p>{c.name}</p>
+              <CharactersNames>{c.name}</CharactersNames>
             </div>
           );
         })}
-      </div>
+      </PageStyled>
     );
   }
 }
--- src/characterliststyled.js
+++ src/characterliststyled.js
@@ -1,13 +1,35 @@
 import styled from 'styled-components';
 import Colors from './Colors';
 
-export const CharacterListStyled = styled.div`
-  height: 400px;
-  /*  background-image: url('https://github.com/ingridinara/pics/blob/main/stars.jpg?raw=true'); */
-  background-color: red;
+export const PageStyled = styled.div`
+  height: 80%vh;
+  background-image: url('https://github.com/ingridinara/pics/blob/main/stars.jpg?raw=true');
   border-top: 1px solid rgb(42, 42, 42);
-
   position: relative;
   z-index: -1;
   padding: 1rem;
+  color: rgb(42, 42, 42);
+`;
+
+export const PageTitle = styled.div`
+  font-size: 12px;
+  color: white;
+  text-align: center;
+`;
+
+export const CharactersNames = styled.div`
+  font-size: 10px;
+  color: rgb(147, 126, 78);
+  padding-bottom: 1rem;
+  background-color: rgb(42, 42, 42);
+  margin-top: 2rem;
+  margin-bottom: 0.1rem;
+  margin-right: 1rem;
+  margin-left: 7rem;
+  padding-left: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid rgb(49, 106, 179);
+  text-align: center;
+  width: 50%;
 `;
